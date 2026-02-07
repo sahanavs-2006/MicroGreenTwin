3db2f15cc4409e9eaaf2ee18820c7f73d325da81
Sahana V S <sahanavs.cs24@rvce.edu.in>
2026-02-07 16:20:29 +0530

    Configure for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react()].filter(Boolean),
-  base: '/MicroGreenTwin/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
